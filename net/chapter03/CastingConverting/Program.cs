056e9fa3f809d75cc6120adf736a5bcfcb73a45c
Victor Vazquez <hugovg799@gmail.com>
2020-07-09 20:40:59 -0500

    Empezando Capitulo 5

--- net/chapter03/CastingConverting/Program.cs
+++ net/chapter03/CastingConverting/Program.cs
@@ -59,6 +59,42 @@ namespace CastingConverting
             object me = new object();
             WriteLine(me.ToString());
 
+            // allocate array of 128 bytes
+            byte[] binaryObject = new byte[128];
+
+            // populate array with random bytes
+            (new Random()).NextBytes(binaryObject);
+
+            WriteLine("Binary object as bytes:");
+
+            for (int index=0; index<binaryObject.Length; index++){
+                Write($"{binaryObject[index]:X}");
+            }
+
+            WriteLine();
+
+            // convert to Base64 string and output as text 
+            string encoded = Convert.ToBase64String(binaryObject);
+            WriteLine($"Binary object as Base64: {encoded}");
+
+            int age = int.Parse("27");
+            DateTime birthday = DateTime.Parse("11 July 1999");
+
+            WriteLine($"I was born {age} ages ago.");
+            WriteLine($"My birthday is {birthday}.");
+            WriteLine($"My birthday is {birthday:D}.");
+
+            WriteLine("How many eggs are there?");
+            int count;
+            string input = Console.ReadLine();            
+            if(int.TryParse(input, out count)){
+                WriteLine($"There are {count} eggs.");
+            }
+            else{
+                WriteLine("I could not parse the input.");
+            }
+            
+
         }
     }
 }
---
+++ net/chapter04/Dumping/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using SharpPad;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Dumping
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var complexObject = new {
+                FirstName = "Petr",
+                BirthDate = new DateTime(
+                    year: 1999, month: 07, day:11
+                ),
+                Friends = new[] {"Amir", "Geoff", "Sal"}
+            };
+
+            WriteLine($"Dumping {nameof(complexObject)} to SharpPad");
+
+            await complexObject.Dump();
+        }
+    }
+}
