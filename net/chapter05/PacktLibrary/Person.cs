9caebc36ef6f8b3e4d216de4ed754f4b4a21bb79
Victor Vazquez <hugovg799@gmail.com>
2020-08-10 18:46:08 -0500

    Storing data within fields

--- net/chapter05/PacktLibrary/Person.cs
+++ net/chapter05/PacktLibrary/Person.cs
@@ -18,5 +18,28 @@ namespace Packt.Shared
 
         public const string Species = "Homo Sapiens";
 
+        // read-only fields
+        // A beter choice for fields that should not change is to mark them as read-only
+        public readonly string HomePlanet = "Earth";
+        // **Good practice**: Use read-only fields over the const field 
+        // for two important reasons: the value can be calculated or loaded at runtime
+        // and can be expressed using any executable statement.
+
+        public readonly DateTime Instantiated;
+
+        // constructors
+        public Person(){
+            // set default values for fields
+            // including read-only fields
+
+            Name = "Unknown";
+            Instantiated = DateTime.Now;
+        }
+
+        public Person(string initialName, string homePlanet) {
+            Name = initialName;
+            HomePlanet = homePlanet;
+            Instantiated = DateTime.Now;
+        }
     }
 }
