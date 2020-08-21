7ca19a66933356780c176c427b6f5e494fee23f6
Victor Vazquez <hugovg799@gmail.com>
2020-08-21 17:08:30 -0500

    Controlling access with properties and indexers

--- net/chapter05/PacktLibrary/PersonAutoGen.cs
+++ net/chapter05/PacktLibrary/PersonAutoGen.cs
@@ -3,5 +3,55 @@ namespace Packt.Shared
     public partial class Person
     {
         
+        // Defining readonly properties
+        // a property defined using C# 1-5 syntax
+        public string Origin{
+            get {
+                return $"{Name} was born on {HomePlanet} xDDD";
+            }
+        }
+
+        // two properties defined using C# lambda expression syntax
+        public string Greeting => $"{Name} says 'Hello!'";
+        public int Age => System.DateTime.Today.Year - DateOfBirth.Year;
+
+        // Defining setteable properties
+        public string FavoriteIceCream {get; set;} // auto-syntax
+
+        private string favoritePrimaryColor;
+
+        public string FavoritePrimaryColor{
+            get {
+                return favoritePrimaryColor;
+            }
+
+            set {
+                switch(value.ToLower()){
+                    case "red":
+                    case "green":
+                    case "blue":
+                        favoritePrimaryColor = value;
+                        break;
+                    default: 
+                        throw new System.ArgumentException(
+                            $"{value} is not a primary color. " +
+                            "Choose from: red, green, blue."
+                        );
+                }
+            }
+        } 
+
+        // Defining indexers
+
+        // indexers
+        public Person this[int index]{
+            get {
+                return Children[index];
+            }
+            set {
+                Children[index] = value;
+            }
+        }
+
     }
 }
--- net/chapter05/PeopleApp/Program.cs
+++ net/chapter05/PeopleApp/Program.cs
@@ -8,132 +8,23 @@ namespace PeopleApp
     {
         static void Main(string[] args)
         {
-            var bob = new Person();
-            bob.Name = "Bob Smith";
-            bob.DateOfBirth = new DateTime(1965, 12, 22);
-
-            WriteLine(
-                format: "{0} was born on {1:dddd, d MMMM yyyy}",
-                arg0: bob.Name,
-                arg1: bob.DateOfBirth
-            );
-
-            var alice = new Person 
-            {
-                Name = "Alice Jones",
-                DateOfBirth = new DateTime(1998, 3, 7)
+            // Defining readonly properties
+            var sam = new Person {
+                Name = "Sam",
+                DateOfBirth = new DateTime(1972, 1, 27)
             };
 
-            WriteLine(
-                format: "{0} was born on {1:dd MMM yy}",
-                arg0: alice.Name,
-                arg1: alice.DateOfBirth
-            );
+            WriteLine(sam.Origin);
+            WriteLine(sam.Greeting);
+            WriteLine(sam.Age);
 
-            bob.FavoriteAncientWonder = WondersOfTheAncientWorld.StatueOfZeusAtOlympia;
+            sam.FavoriteIceCream = "Chocolate Fudge";
 
-            WriteLine(format: "{0}'s favorite wonder is {1}. It's integer is {2}.",
-            arg0: bob.Name,
-            arg1: bob.FavoriteAncientWonder,
-            arg2: (int)bob.FavoriteAncientWonder + 1
-            );
+            WriteLine($"Sam's favorite ice-cream flavor is {sam.FavoriteIceCream}");
 
-            bob.BucketList = WondersOfTheAncientWorld.HangingGardensOfBabylon | WondersOfTheAncientWorld.MausoleumAtHalicarnassus;
+            sam.FavoritePrimaryColor = "Red";
 
-            WriteLine($"{bob.Name}'s Bucket list is {bob.BucketList}");
-
-            bob.Children.Add(new Person {Name = "Alfred"});
-            bob.Children.Add(new Person {Name = "Zoe"});
-
-            for (int child = 0; child < bob.Children.Count; child++ ) {
-                WriteLine($"{bob.Children[child].Name}");
-            }
-
-            BankAccount.InterestRate = 0.012M;  // store a shared member
-
-            var jonesAccount = new BankAccount();
-            jonesAccount.AccountName = "Mrs. Jones";
-            jonesAccount.Balance = 2400;
-
-            WriteLine(format: "{0} earned {1:C} interest",
-            arg0: jonesAccount.AccountName,
-            arg1: jonesAccount.Balance * BankAccount.InterestRate);
-
-            var gerrierAccount = new BankAccount();
-            gerrierAccount.AccountName = "Ms. Gerrier";
-            gerrierAccount.Balance = 98;
-
-            WriteLine(format: "{0} earned {1:C} interest",
-            arg0: gerrierAccount.AccountName,
-            arg1: gerrierAccount.Balance * BankAccount.InterestRate);
-
-            WriteLine($"{bob.Name} is a {Person.Species}");
-            WriteLine($"{bob.Name} was born on {bob.HomePlanet}");
-
-            var blankPerson = new Person();
-
-            WriteLine(format: "{0} of {1} was created at {2:hh:mm:ss} on a {2:dddd}.",
-            arg0: blankPerson.Name,
-            arg1: blankPerson.HomePlanet,
-            arg2: blankPerson.Instantiated);
-
-            var gunny = new Person("Gunny", "Mars");
-
-            WriteLine(format: "{0} of {1} was created at {2:hh:mm:ss} on a {2:dddd}.",
-            arg0: gunny.Name,
-            arg1: gunny.HomePlanet,
-            arg2: gunny.Instantiated);
-
-            bob.WriteToConsole();
-            WriteLine(bob.GetOrigin());
-
-            (string, int) fruit = bob.GetFruit();
-            WriteLine($"{fruit.Item1}, {fruit.Item2} there are.");
-
-            var fruitNamed = bob.GetNamedFruit();
-            WriteLine($"There are {fruitNamed.Number} {fruitNamed.Name}");
-
-            // Infering tuple names
-            var thing1 = ("Neville", 4);
-            WriteLine($"{thing1.Item1} has {thing1.Item2} children");
-
-            var thing2 = (bob.Name, bob.Children.Count);
-            WriteLine($"{thing2.Name} has {thing2.Count} children");
-
-            // Deconstructing tuples
-            (string fruitName, int fruitNumber) = bob.GetFruit();
-            WriteLine($"Deconstructed: {fruitName}, {fruitNumber}");
-
-            // Defining and passing parameters to methods
-            WriteLine(bob.SayHello());
-            WriteLine(bob.SayHello("Keko"));
-
-            // Passing optional parameters and naming arguments
-            WriteLine(bob.OptionalParameters());
-            WriteLine(bob.OptionalParameters("Jump!", 98.5));
-            WriteLine(bob.OptionalParameters(number: 52.7, command: "Hide!"));
-
-            // Controlling how parameters are passed
-            int a = 10;
-            int b = 20;
-            int c = 30;
-
-            WriteLine($"Before: a = {a}, b = {b}, c = {c}");
-
-            bob.PassingParameters(a, ref b, out c);
-
-            WriteLine($"After: a = {a}, b = {b}, c = {c}");
-
-            int d = 10;
-            int e = 20;
-
-            WriteLine(
-            $"Before: d = {d}, e = {e}, f doesn't exist yet!");
-
-            // simplified C# 7.0 syntax for the out parameter
-            bob.PassingParameters(d, ref e, out int f);
-
-            WriteLine($"After: d = {d}, e = {e}, f = {f}");
+            WriteLine($"Sam's favorite primart color is {sam.FavoritePrimaryColor}");
 
         }
     }
